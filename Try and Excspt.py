71ea5f7f5ac4dcd7f47b7b1bf68860b54f8e79ee
Abdulwahab1313 <ahmedabdulwahab1212@gmail.com>
2025-09-14 13:08:16 +0100

    Update Try and Excspt.py

--- Try and Excspt.py
+++ Try and Excspt.py
@@ -21,20 +21,21 @@ if fl:
     with pd.ExcelWriter(fs, engine="openpyxl") as writer:
    		clean.to_excel(writer, index=False)
    		pick = st.radio("Pick what type of Format you'll like to download",["Excel","csv"])
-        if pick == "Excel":
+   		if pick == "Excel":
             btn = st.download_button(
                 label="click here to download the clean data",
                 data=buffer,
                 file_name= st.text_input("Enter your desired name") +".xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
-        elif pick == "csv":
+		elif pick == "csv":
             tb2 = clean.to_csv()
             btn = st.download_button(
                 label = "click to download the clean csv ",
                 data = tb2,
                 file_name = st.text_input("Enter your desired name") + ".csv"
             )
+
 
 
 
